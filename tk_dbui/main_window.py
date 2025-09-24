e4c1950d6ce47381ee4fd23b1f4252204d98a561
Mickael Garcia <mgarcia@2minutes.fr>
2025-09-24 14:03:46 +0200

    [ADD] Add radio button for asset/db options

--- tk_dbui/main_window.py
+++ tk_dbui/main_window.py
@@ -36,18 +36,44 @@ class MainWidget(qtw.QWidget):
         super().__init__(*args, **kwargs)
         self.app = app
 
+        # Widgets
         self._cbx_project = qtw.QComboBox(self)
         self._project_model = ProjectListModel()
         self._project_model.set_projects(list(self.app.db.projects()))
         self._cbx_project.setModel(self._project_model)
 
-        lay_master = qtw.QHBoxLayout(self)
+        self._btn_asset = qtw.QPushButton("Asset")
+        self._btn_asset.setCheckable(True)
 
+        self._btn_db = qtw.QPushButton("Db")
+        self._btn_db.setCheckable(True)
+
+        # Layouts
+        lay_master = qtw.QHBoxLayout(self)
         lay_base_content = qtw.QVBoxLayout()
+        lay_btn = qtw.QHBoxLayout()
+
+        lay_btn.addWidget(self._btn_asset)
+        lay_btn.addWidget(self._btn_db)
 
         lay_base_content.addWidget(self._cbx_project)
+        lay_base_content.addLayout(lay_btn)
 
         lay_master.addLayout(lay_base_content)
+
+        # Connections
+        self._btn_asset.clicked.connect(self._on_btn_asset_clicked)
+        self._btn_db.clicked.connect(self._on_btn_db_clicked)
+
+    def _on_btn_asset_clicked(self):
+        is_checked = self._btn_asset.isChecked()
+        if is_checked:
+            self._btn_db.setChecked(False)
+
+    def _on_btn_db_clicked(self):
+        is_checked = self._btn_db.isChecked()
+        if is_checked:
+            self._btn_asset.setChecked(False)
 
 
 if __name__ == "__main__":
